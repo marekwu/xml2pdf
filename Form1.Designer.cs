097f399b4960e09fb7e7d3af8edb589d6fe32583
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-09-06 10:09:03 +0200

    First working version.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace xml2pdf_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChangeFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labFolder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 106);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(794, 564);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(731, 25);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 75);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "&Zakończ";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(549, 25);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(176, 75);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "&Drukuj";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnChangeFolder
+            // 
+            this.btnChangeFolder.Location = new System.Drawing.Point(468, 25);
+            this.btnChangeFolder.Name = "btnChangeFolder";
+            this.btnChangeFolder.Size = new System.Drawing.Size(75, 75);
+            this.btnChangeFolder.TabIndex = 3;
+            this.btnChangeFolder.Text = "&Folder";
+            this.btnChangeFolder.UseVisualStyleBackColor = true;
+            this.btnChangeFolder.Click += new System.EventHandler(this.btnChangeFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(21, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // labFolder
+            // 
+            this.labFolder.AutoSize = true;
+            this.labFolder.Location = new System.Drawing.Point(13, 25);
+            this.labFolder.Name = "labFolder";
+            this.labFolder.Size = new System.Drawing.Size(67, 13);
+            this.labFolder.TabIndex = 5;
+            this.labFolder.Text = "Folder XML: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 673);
+            this.Controls.Add(this.labFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChangeFolder);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Drukarka XML\'i";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnChangeFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labFolder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+
+
+namespace xml2pdf_win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+
+            String katalog;
+            String sciezka;
+
+
+
+            // katalog = "D:\\XML\\pharma-art\\";
+
+            katalog = "d:\\xml";
+
+            //string[] filePaths = Directory.GetFiles(@katalog);
+
+            string[] filePaths = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+
+            int licznik = 0;
+
+            //  String element = filePaths[1];
+
+            foreach (String element in filePaths)
+
+            {
+                licznik++;
+
+
+
+                sciezka = "file://" + element;
+
+
+                label1.Text = "Plik przetwarzany " + licznik.ToString() + " : " + element;
+
+
+                webBrowser.Navigate(new Uri(sciezka));
+
+
+
+                //webBrowser.Print();
+
+
+                while (webBrowser.IsBusy)
+                    System.Windows.Forms.Application.DoEvents();
+                for (int i = 0; i < 5000; i++)
+                    if (webBrowser.ReadyState != System.Windows.Forms.WebBrowserReadyState.Complete)
+                    {
+                        System.Windows.Forms.Application.DoEvents();
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    else
+                        break;
+
+
+                System.Threading.Thread.Sleep(1000);
+
+                //MessageBox.Show("test: ", element );
+
+                //Thread.Sleep(6000);
+
+
+                webBrowser.Print();
+
+
+                while (webBrowser.IsBusy)
+                    System.Windows.Forms.Application.DoEvents();
+                for (int i = 0; i < 5000; i++)
+                    if (webBrowser.ReadyState != System.Windows.Forms.WebBrowserReadyState.Complete)
+                    {
+                        System.Windows.Forms.Application.DoEvents();
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    else
+                        break;
+
+
+                System.Threading.Thread.Sleep(1000);
+
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+
+        }
+
+        private void btnChangeFolder_Click(object sender, EventArgs e)
+        {
+
+            folderBrowserDialog1.ShowDialog();
+            labFolder.Text = "Folder z XML:  " + folderBrowserDialog1.SelectedPath.ToString();
+
+
+        }
+    }
+    
+   
+
+
+
+}
